13ff11bee0736f423d0f8a4665985395f32c8409
xan2301 <2silverghost2@gmail.com>
2023-04-30 15:22:50 +0200

    works

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,16 +1,52 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {AppService} from "./app.service";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'frontend-jwt';
-  email: any;
-  password: any;
-  login(){
+  email = '';
+  password = '';
+  data: Data[];
 
+
+  ngOnInit() {
+  }
+
+  constructor(private appService: AppService) {
+    this.data = [];
+  }
+
+  login() {
+
+    console.log('email ' + this.email);
+
+    let payload = {email: this.email, password: this.password};
+
+    this.appService.loginApi(payload).subscribe((res: any) => {
+      console.log(res);
+      this.getData(res.accessToken);
+
+    })
 
   }
+
+  getData(token: string)
+  {
+    this.appService.getData(token).subscribe((res: any) => {
+    this.data = res;
+  })
+
+  }
+}
+
+export interface Data{
+
+  title: string;
+  author: string;
+
+
 }
